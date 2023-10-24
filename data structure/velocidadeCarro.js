e5359e691c7a8693b25d5dd9e1da32951c86eb73
marcosvale <marcosvaledev@gmail.com>
2023-10-24 20:55:51 -0300

    news codes

---
+++ data structure/velocidadeCarro.js
@@ -0,0 +1,8 @@
+let velocidadeMaxima = 60;
+let velocidadeUsuario = Number(prompt('qual foi sua velocidade?'));
+
+if ( velocidadeUsuario > velocidadeMaxima){
+    alert ('voce ultrapassou a velocidade maxima permitida!');
+} else{
+    alert('dentro da velocidade');
+}
---
+++ pratice/data types/ex01.js
@@ -0,0 +1,3 @@
+console.log('be eveloper')
+console.log("be developer")
+console.log((typeof`be developer`));
