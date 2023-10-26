7e21151945ee1c5ba53c07bf872d4413b94744c1
marcosvale <marcosvaledev@gmail.com>
2023-10-26 07:10:30 -0300

    update files

---
+++ ex011.js
@@ -0,0 +1,11 @@
+let age = 18
+let CNH = true
+
+if( age >= 18 && CNH == true ){
+    console.log('voce é maior de idade e possui CNH');
+}else if( age >= 18 && CNH == false){
+    console.log('voce é maior de idade, mas precisa tirar sua CNH');
+}else{
+    console.log('voce nao pode dirigir');
+}
+
