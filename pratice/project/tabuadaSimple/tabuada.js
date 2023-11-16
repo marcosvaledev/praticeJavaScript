121fe4c40f747762e292ee48fe2cc6ba3055252c
marcosvaledev <111076997+marcosvaledev@users.noreply.github.com>
2023-11-16 13:13:46 -0300

    news pratices

---
+++ pratice/project/tabuadaSimple/tabuada.js
@@ -0,0 +1,7 @@
+let i = 0;
+let tabuadaPedida = Number(prompt('What number you want calcule?'))
+
+for(let i = 0;i <= 1000; i++){
+let res = 'A tabuada de: ' + tabuadaPedida + ' x ' + i + ' = ' + (tabuadaPedida * i);
+  console.log(res)
+}
